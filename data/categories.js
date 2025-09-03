021abfbd32fc40ac450e1ccdc3f835094dee65c3
themark712 <93409384+themark712@users.noreply.github.com>
2025-09-03 07:44:24 -0500

    Add files via upload

---
+++ data/categories.js
@@ -0,0 +1,22 @@
+const categories = [
+  {
+    id: 1,
+    name: "M*A*S*H"
+  },
+  {
+    id: 2,
+    name: "Stephen King"
+  },
+  {
+    id: 3,
+    name: "American History"
+  },
+  {
+    id: 4,
+    name: "World Captials"
+  },
+  {
+    id: 5,
+    name: "U.S. Captials"
+  }
+];
---
+++ data/questions.js
@@ -0,0 +1,68 @@
+const questions = [
+  {
+    id: 1,
+    categoryid: 1,
+    question: 'Which character is known as "Hawkeye"?',
+    answers: [
+      { text: 'Hunnicut', correct: false },
+      { text: 'Pierce', correct: true },
+      { text: 'Potter', correct: false },
+      { text: 'Burns', correct: false }
+    ]
+  },
+  {
+    id: 2,
+    categoryid: 2,
+    question: 'Where is Roland (from The Dark Tower series) from?',
+    answers: [
+      { text: 'Gilead', correct: true },
+      { text: 'Mejis', correct: false },
+      { text: 'Lud', correct: false },
+      { text: 'New York', correct: false }
+    ]
+  },
+  {
+    id: 3,
+    categoryid: 3,
+    question: 'Which was NOT a battle in the American Revolution?',
+    answers: [
+      { text: 'Yorktown', correct: false },
+      { text: 'Saratoga', correct: false },
+      { text: 'Bunker Hill', correct: false },
+      { text: 'Perryville', correct: true }
+    ]
+  },
+  {
+    id: 4,
+    categoryid: 4,
+    question: 'What is the captial of Northern Ireland?',
+    answers: [
+      { text: 'Dublin', correct: false },
+      { text: 'Belfast', correct: true },
+      { text: 'Derry', correct: false },
+      { text: 'Newcastle', correct: false }
+    ]
+  },
+  {
+    id: 5,
+    categoryid: 5,
+    question: 'What is the captial of Kentucky?',
+    answers: [
+      { text: 'Louisville', correct: false },
+      { text: 'Lexington', correct: false },
+      { text: 'Frankfort', correct: true },
+      { text: 'Bowling Green', correct: false }
+    ]
+  },
+  {
+    id: 6,
+    categoryid: 1,
+    question: 'Who nailed Hawkeye\'s boot to the tent floor?',
+    answers: [
+      { text: 'B.J.', correct: true },
+      { text: 'Winchester', correct: false },
+      { text: 'Margaret', correct: false },
+      { text: 'Frank', correct: false }
+    ]
+  }
+];
